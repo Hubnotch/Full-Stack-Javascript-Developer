ff14714446b53199bbe35a4ef0160d8d03af042c
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2022-12-12 08:50:38 +0100

    testing of all  utility functions

--- Tesing-with-jasmine/dist/tests/indexSpec.js
+++ Tesing-with-jasmine/dist/tests/indexSpec.js
@@ -1,9 +1,43 @@
 "use strict";
+// import myFunc from '../index'
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = __importDefault(require("../index"));
-it('expect myFunc(5) to eqaul 25', () => {
-    expect((0, index_1.default)(5)).toEqual(25);
+// it('expect myFunc(5) to eqaul 25', () => {
+//     expect(myFunc(5)).toEqual(25);
+// });
+const numbers_1 = __importDefault(require("../utilities/numbers"));
+const numbers_2 = __importDefault(require("../utilities/numbers"));
+const numbers_3 = __importDefault(require("../utilities/numbers"));
+const arrays_1 = __importDefault(require("../utilities/arrays"));
+const arrays_2 = __importDefault(require("../utilities/arrays"));
+const arrays_3 = __importDefault(require("../utilities/arrays"));
+it('add two arrays', () => {
+    const arr1 = [1, 2, 3];
+    const arr2 = ['a', 'b', 'c'];
+    const res = arrays_3.default.concatArr(arr1, arr2);
+    expect(res).toEqual([1, 2, 3, 'a', 'b', 'c']);
+});
+it('add elements of an array', () => {
+    const arr1 = [1, 2, 3];
+    const res = arrays_2.default.addArr(arr1);
+    expect(res).toEqual(6);
+});
+it('return largest elements of an array', () => {
+    const arr1 = [1, 2, 3];
+    const res = arrays_1.default.lgNum(arr1);
+    expect(res).toBeTruthy();
+});
+it('return true for positive numbers', () => {
+    const res = numbers_3.default.square(4);
+    expect(res).toBeGreaterThan(4);
+});
+it('return false', () => {
+    const res = numbers_1.default.multiply(0, 8);
+    expect(res).toBeFalsy();
+});
+it('return true for positive numbers', () => {
+    const res = numbers_2.default.sum(4, 4);
+    expect(res).toBeGreaterThanOrEqual(4);
 });
--- Tesing-with-jasmine/src/tests/indexSpec.ts
+++ Tesing-with-jasmine/src/tests/indexSpec.ts
@@ -4,4 +4,81 @@ it('expect myFunc(5) to eqaul 25', () => {
     expect(myFunc(5)).toEqual(25);
 });
 
+import arrays from '../utilities/arrays';
+import numbers from '../utilities/numbers';
+import strings from '../utilities/strings';
+
+const numArr = [3, 4, 5, 6];
+const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
+
+it('should capitalize a string', () => {
+    expect(strings.capitalize('a sentence')).toEqual('A Sentence');
+});
+
+it('should be a sum greater than 10', () => {
+    expect(numbers.sum(3, 10)).toBeGreaterThan(10);
+});
+
+it('multiply 3 by 5 and be 15', () => {
+    expect(numbers.multiply(3, 5)).toEqual(15);
+});
+
+it('should add numbers in array and be truthy', () => {
+    expect(arrays.addArr(numArr)).toBeTruthy();
+});
+
+it('should concatenate 2 arrays to not equal just 1', () => {
+    expect(arrays.concatArr(numArr, wordArr)).not.toEqual(numArr);
+});
+
+it('should not contain the third index', () => {
+    expect(arrays.cut3(wordArr)).not.toContain('rabbit');
+});
+
+it('should not have a large number and be falsy', () => {
+    expect(arrays.lgNum(wordArr)).toBeFalsy();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// it('add two arrays', () => {
+//     const arr1 = [1, 2, 3]
+//     const arr2 = ['a', 'b', 'c']
+//     const res = arrays.concatArr(arr1, arr2)
+//     expect(res).toEqual([1, 2, 3, 'a', 'b', 'c'])
+// })
+// it('add elements of an array', () => {
+//     const arr1 = [1, 2, 3]
+//     const res = arrays.addArr(arr1)
+//     expect(res).toEqual(6)
+// })
+// it('return largest elements of an array', () => {
+//     const arr1 = [1, 2, 3]
+//     const res = arrays.lgNum(arr1)
+//     expect(res).toBeTruthy()
+// })
+// it('return true for positive numbers', () => {
+//     const res = numbers.square(4)
+//     expect(res).toBeGreaterThan(4)
+// })
+// it('return false', () => {
+//     const res = numbers.multiply(0,8);
+//     expect(res).toBeFalsy();
+// })
+// it('return true for positive numbers', () => {
+//     const res = numbers.sum(4, 4)
+//     expect(res).toBeGreaterThanOrEqual(4)
+// })
 
